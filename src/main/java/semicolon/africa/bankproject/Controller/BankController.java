d1e06a594caf9dc940820c08f0d25e4d3db0d0cb
Ololade0 <adesuyiololade@gmail.com>
2022-09-26 10:12:53 +0100

    bank project

--- src/main/java/semicolon/africa/bankproject/Controller/BankController.java
+++ src/main/java/semicolon/africa/bankproject/Controller/BankController.java
@@ -15,14 +15,14 @@ public class BankController {
     @Autowired
     private BankService bankService;
 
-//    @PostMapping("/banks")
-//    public ResponseEntity<?> registerBank(@RequestBody BankRegisterRequest bankRegisterRequest){
-//        try {
-//        BankRegisterResponse bankRegisterResponse = bankService.registerBank(bankRegisterRequest);
-//        return new ResponseEntity<>(bankRegisterResponse, HttpStatus.CREATED);
-//        }
-//        catch (BankDoesNotExistException e){
-//            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
-//        }
-//    }
+    @PostMapping("/banks")
+    public ResponseEntity<?> registerBank(@RequestBody BankRegisterRequest bankRegisterRequest){
+        try {
+        BankRegisterResponse bankRegisterResponse = bankService.registerBank(bankRegisterRequest);
+        return new ResponseEntity<>(bankRegisterResponse, HttpStatus.CREATED);
+        }
+        catch (BankDoesNotExistException e){
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+    }
 }
